63c521aaac11d58838d00998c1890c0a6177ab68
Govindaraj Cholappagol <govindaraj5547@gmail.com>
2026-05-25 11:46:52 +0530

    updated description

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -10,11 +10,6 @@ const Header = () => {
       <div className="body-container">
         <img src={Settings.profile_photo} alt="profile_pic" />
         <p className="header_para">
-          {/* I am a dedicated and detail-oriented Full Stack Developer with 1 year of hands-on experience in designing, developing, and deploying web applications. Proficient in modern technologies like ReactJS, NextJS, and JavaScript, I specialize in creating dynamic, user-friendly interfaces. My backend expertise includes NodeJS and ExpressJS, along with strong SQL database management skills.
-       
-        I have a solid foundation in HTML, CSS, and responsive web design, ensuring seamless user experiences across devices. With practical knowledge of hosting platforms like Hostinger and GoDaddy, I am adept at deploying and maintaining websites.
-        
-         Passionate about problem-solving and delivering high-quality solutions, I excel in collaborative environments and take pride in building efficient, scalable, and robust applications that meet user needs. */}
           I am a passionate Full Stack Developer with 3 years of experience in
           building responsive and modern web applications using React.js,
           Next.js, JavaScript, Node.js, and Express.js. I specialize in creating
